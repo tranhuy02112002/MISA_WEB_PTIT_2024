f846561cc8714ccd64d1a821f8afbd37ebf80731
HuyBBC <tranquanghuy02112002@gmail.com>
2025-06-14 22:58:04 +0700

    update code lần 3

--- BE/Web.Misaweb2024/Web.Misaweb2024.Api/obj/Debug/net8.0/Web.Misaweb2024.Api.AssemblyInfo.cs
+++ BE/Web.Misaweb2024/Web.Misaweb2024.Api/obj/Debug/net8.0/Web.Misaweb2024.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web.Misaweb2024.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40c5f62a5870aa9ca69f7da190843248d75d2c8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84af8295649533ab56b0a4f628dc7b60bc37b7a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web.Misaweb2024.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web.Misaweb2024.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
